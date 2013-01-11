fea9b87994b2fedb7df1aaf9e4fa1a87400b0025
peterricci <peterricci@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-11 10:53:03 +0000

    Y Google+ Hangout Events 0.0.2
    git-svn-id: https://plugins.svn.wordpress.org/yakadanda-google-hangout-events/tags/0.0.2@651243 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ oauth2callback.php
@@ -0,0 +1,29 @@
+<?php
+include_once dirname( __FILE__ ) . "/../../../wp-load.php";
+require_once( dirname( __FILE__ ) . '/src/Google_Client.php');
+require_once( dirname( __FILE__ ) . '/src/contrib/Google_CalendarService.php');
+
+$data = get_option('yakadanda_googleplus_hangout_event_options');
+
+if( !defined('GPLUS_HANGOUT_EVENT_URL') ) {
+  define( 'GPLUS_HANGOUT_EVENT_URL', plugins_url(null, __FILE__) );
+}
+
+$client = new Google_Client();
+$client->setApplicationName("Yakadanda GooglePlus Hangout Event");
+
+// Visit https://code.google.com/apis/console?api=calendar to generate your
+// client id, client secret, and to register your redirect uri.
+$client->setClientId( $data['client_id'] );
+$client->setClientSecret( $data['client_secret'] );
+$client->setRedirectUri( GPLUS_HANGOUT_EVENT_URL . '/oauth2callback.php' );
+$client->setScopes( 'https://www.googleapis.com/auth/calendar.readonly' );
+$client->setDeveloperKey( $data['api_key'] );
+
+if (isset($_GET['code'])) {
+  $client->authenticate($_GET['code']);
+  $option = 'yakadanda_googleplus_hangout_event_access_token';
+  update_option( $option, $client->getAccessToken() );
+}
+
+wp_redirect( admin_url( 'options-general.php?page=googleplus-hangout-event' ) ); exit;
