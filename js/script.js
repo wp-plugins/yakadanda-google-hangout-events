6f484ca63c859182b4e95ef915702816dffd3921
peterricci <peterricci@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-25 03:12:57 +0000

    y-g+hangout-events v0.0.4
    git-svn-id: https://plugins.svn.wordpress.org/yakadanda-google-hangout-events/trunk@658459 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/script.js
@@ -0,0 +1,103 @@
+jQuery(function($){
+  
+  // In admin page settings
+  if ( $('body').find('#google-hangout-event').length == 1 ) {
+    // select display
+    var value_of_display = $('#hidden_display').val();
+    if ( value_of_display ) $('#display option[value=' + value_of_display + ']').attr('selected',true);
+    
+    // select countdown
+    var  value_of_countdown_display = $('#hidden_countdown_display').val();
+    if ( value_of_countdown_display ) $('#countdown_display :radio[value=' + value_of_countdown_display + ']').attr('checked',true);
+    
+    // select themes, sizes, and styles
+    var optionsFirst = new Array("title", "date", "detail", "icon", "countdown"),
+    optionsLast = new Array("theme", "size", "style");
+    
+    for ( f in optionsFirst ) {
+      for ( l in optionsLast ) {
+        var the_value = $('#hidden_'+optionsFirst[f]+'_'+optionsLast[l]).val();
+        if ( the_value ) $('#'+optionsFirst[f]+'_'+optionsLast[l]+' option[value="' + the_value + '"]').attr('selected',true);
+      }
+    }
+    
+    // Farbtastic Color Picker
+    var f = $.farbtastic('#picker');
+    var p = $('#picker').css('opacity', 0.25);
+    var selected;
+    
+    $('.colorwheel').each(function () { f.linkTo(this); $(this).css('opacity', 0.75); }).focus(function() {
+      if (selected) {
+        $(selected).css('opacity', 0.75).removeClass('colorwell-selected');
+      }
+      f.linkTo(this);
+      p.css('opacity', 1);
+      $(selected = this).css('opacity', 1).addClass('colorwell-selected');
+    });
+    
+  }
+  
+  /* In frontend */
+  // 1st widget
+  if ( $('body').find('#ghe-1st-widget').length == 1 ) {
+    var gheStartTimes1st = $('#ghe-start-times-1st').val();
+    if (typeof gheStartTimes1st != "undefined") {
+      startTimes1st = gheStartTimes1st.split(';');
+      for (i in startTimes1st) {
+        if ( $('body').find('#ghe-countdown-1st-'+i).length == 1 ) {
+          loadCountDown(i, startTimes1st[i], 'ghe-countdown-1st-');
+        }
+      }
+    }
+  }
+  // 2nd widget
+  if ( $('body').find('#ghe-2nd-widget').length == 1 ) {
+    var gheStartTimes2nd = $('#ghe-start-times-2nd').val();
+    if (typeof gheStartTimes2nd != "undefined") {
+      startTimes2nd = gheStartTimes2nd.split(';');
+      for (i in startTimes2nd) {
+        if ( $('body').find('#ghe-countdown-2nd-'+i).length == 1 ) {
+          loadCountDown(i, startTimes2nd[i], 'ghe-countdown-2nd-');
+        }
+      }
+    }
+  }
+  
+});
+
+function loadCountDown(i, startTime, selector) {
+  jQuery(function($){
+    var theSelector = selector + i;
+    $("#"+theSelector).countdown({
+      htmlTemplate: "%d<span>&nbsp;Days</span><em>|</em>%h<span>&nbsp;Hours</span><em>|</em>%i<span>&nbsp;Minutes</span><em>|</em>%s<span>&nbsp;Seconds</span>",
+      date: startTime,
+      yearsAndMonths: true,
+      /*servertime: function() { 
+        var time = null; 
+        $.ajax({url: 'get_time.php', 
+          async: false, 
+          dataType: 'text', 
+          success: function( data, status, xhr ) {  
+            time = data; 
+          }, 
+          error: function(xhr, status, err) { 
+            time = new Date(); 
+            time = time.getTime();
+          }
+        });
+        return time; 
+      },*/
+      hoursOnly: false,
+      onComplete: function( event ) {
+        $(this).html("Completed");
+      },
+      onPause: function( event, timer ) {
+        $(this).html("Pause");
+      },
+      onResume: function( event ) {
+        $(this).html("Resumed");
+      },
+      leadingZero: true
+    });
+  });
+}
---
+++ readme.txt
@@ -0,0 +1,71 @@
+=== Yakadanda Google+ Hangout Events ===
+Contributors: Yakadanda.com
+Donate link: http://www.yakadanda.com/
+Tags: Google, Hangout, Hangouts, Events, Google+, Googleplus, Calendar, Calendars, Yakadanda
+Requires at least: 3.4.0
+Tested up to: 3.5
+Stable tag: 0.0.4
+License: GPLv2 or later
+
+Generate linked Google+ Hangout Event notifications in a widget from a Google Calendar with a nifty Countdown Clock to each event.
+
+== Description ==
+
+This plugin will generate linked Google+ Hangout Event notifications in a widget from a Google Calendar with a nifty Countdown Clock to each event. Fully customizable with fonts and colors.
+
+= Features =
+* Choose # of Events to Display on the widget
+* Choose colors, fonts, sizes, and styles for the widget
+* Links Directly to Hangout Event on Google+
+* Show Google+ Events on your post or page
+
+== Installation ==
+
+1. Upload the full directory into your wp-content/plugins directory
+2. Activate the plugin at the plugin administration page
+3. Configure Google+ Hangout Events using the following pages in the admin panel: Settings -> Google+ Hangout Events.
+
+That's it your done, you can add the Google Plus Hangout Events Widget to any sidebar.
+
+== Frequently Asked Questions ==
+
+= Do I need an account on Google? =
+You only need an Google+ account.
+
+== Screenshots ==
+
+
+== Changelog ==
+
+= 0.0.4 =
+* Added new widgets for normal event type
+* Added google+ events shortcode
+* Added time ago function for event create or event updated on the shortcode
+* Added google maps link with query search on the shortcode for normal event
+* Added Google fonts
+
+= 0.0.3 =
+* Added 2nd widget as widget (extra)
+* Added countdown option feature for 2nd widget
+* Added logout action
+
+= 0.0.2 =
+* Updated to Google Calendar API v3
+* Added instructions page as manual
+* Displayed the connection status
+
+= 0.0.1 =
+* Used calendar feed in Google Calendar API v2
+
+== Upgrade Notice ==
+
+= 0.0.4 =
+* Improves the event time
+* Fixes message widget if no hangout event or no normal event
+
+= 0.0.3 =
+* Fixes bugs in widget if google calendar not connected
+
+= 0.0.2 =
+* Fixes blank issues in widget if browser installed Addllock add-on
+* Fixes bugs if no hangout event
