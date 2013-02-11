df35b23740c475e87a0ce0ffad7e32f1ee75c0fc
peterricci <peterricci@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-02-11 09:22:30 +0000

    v0.0.5 copy to tags
    git-svn-id: https://plugins.svn.wordpress.org/yakadanda-google-hangout-events/tags/0.0.5@666181 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ yakadanda-googleplus-hangout-events.php
@@ -0,0 +1,74 @@
+<?php
+/*
+Plugin Name: Yakadanda Google+ Hangout Events
+Plugin URI: http://www.yakadanda.com/plugins/yakadanda-google-hangout-events/
+Description: A countdown function to time of the Google+ Hangout Events.
+Version: 0.0.5
+Author: Peter Ricci
+Author URI: http://www.yakadanda.com/
+License: GPL2
+*/
+
+/* Put setup procedures to be run when the plugin is activated in the following function */
+function googleplushangoutevent_activate() {
+  if ( ! get_option('yakadanda_googleplus_hangout_event_options') )
+    add_option('yakadanda_googleplus_hangout_event_options', null, false, false);
+  if ( ! get_option('yakadanda_googleplus_hangout_event_access_token') )
+    add_option('yakadanda_googleplus_hangout_event_access_token', null, false, false);
+}
+register_activation_hook( __FILE__, 'googleplushangoutevent_activate' );
+
+// On deacativation, clean up anything your component has added.
+function googleplushangoutevent_deactivate() {
+	// You might want to delete any options or tables that your component created.
+  
+}
+register_deactivation_hook( __FILE__, 'googleplushangoutevent_deactivate' );
+
+if( !defined('GPLUS_HANGOUT_EVENT_URL') ) {
+  define( 'GPLUS_HANGOUT_EVENT_URL', plugins_url(null, __FILE__) );
+}
+
+// Register scripts & styles
+add_action( 'init', 'googleplushangoutevent_register' );
+function googleplushangoutevent_register() {
+  /* Styles */
+  // Google web fonts
+  $google_fonts = googleplushangoutevent_google_fonts();
+  if ($google_fonts) wp_register_style( 'googleplushangoutevent-google-fonts', 'http://fonts.googleapis.com/css?family=' . $google_fonts, false, '0.0.4', 'all' );
+  // Yakadanda GooglePlus Hangout Event style
+  wp_register_style( 'googleplushangoutevent-style', GPLUS_HANGOUT_EVENT_URL . '/css/style.css', false, '0.0.4', 'all' );
+  
+  /* Scripts */
+  // Countdown timer jQuery Plugin
+  wp_register_script( 'googleplushangoutevent-countdown', GPLUS_HANGOUT_EVENT_URL . '/js/jquery.jcountdown.min.js', array('jquery'), '1.4.2', true );
+  // Google+ Hangout Event script
+  wp_register_script( 'googleplushangoutevent-script', GPLUS_HANGOUT_EVENT_URL . '/js/script.js', array('googleplushangoutevent-countdown'), '0.0.4', true );
+}
+
+// Call stylesheets
+function googleplushangoutevent_admin_enqueue_styles() {
+  wp_enqueue_style( 'farbtastic' );
+}
+add_action( 'wp_enqueue_scripts', 'googleplushangoutevent_wp_enqueue_styles' );
+function googleplushangoutevent_wp_enqueue_styles() {
+  $google_fonts = googleplushangoutevent_google_fonts();
+  if ($google_fonts) wp_enqueue_style( 'googleplushangoutevent-google-fonts' );
+  wp_enqueue_style( 'googleplushangoutevent-style' );
+}
+
+// Call javascripts
+function googleplushangoutevent_admin_enqueue_scripts() {
+  wp_enqueue_script( 'farbtastic' );
+  wp_enqueue_script( 'googleplushangoutevent-script' );
+}
+add_action( 'wp_enqueue_scripts', 'googleplushangoutevent_wp_enqueue_scripts' );
+function googleplushangoutevent_wp_enqueue_scripts() {
+  wp_enqueue_script( 'jquery' );
+  wp_enqueue_script( 'googleplushangoutevent-countdown' );
+  wp_enqueue_script( 'googleplushangoutevent-script' );
+}
+
+require_once( dirname( __FILE__ ) . '/admin/functions.php');
+require_once( 'yakadanda-widgets.php' );
+require_once( 'yakadanda-shortcode.php' );
---
+++ yakadanda-shortcode.php
@@ -0,0 +1,162 @@
+<?php
+/*
+ * [google+events]
+ * [google+events type="all" limit="6" past="8"]
+ * type = all, normal, or hangout, default is all
+ * limit = number of events to show, it limited to 20
+ * past = number of months, to display past events in X months ago
+ * author = self, or all, default is self
+ */
+function googleplushangoutevent_shortcode( $atts ) {
+  extract( shortcode_atts( array(
+    'type' => 'all',
+    'limit' => 20,
+    'past' => null,
+    'author' => 'self'
+  ), $atts ) );
+  
+  if ($limit > 20) $limit = 20;
+  
+  $events = googleplushangoutevent_response( $past );
+  $output = 'No Event.';
+  $i = 0;
+  $filter = true;
+  $creator = 1;
+  
+  if ($events) {
+    $output = null;
+    foreach ($events as $event) {
+      $hangoutlink = isset($event['hangoutLink']) ? $event['hangoutLink'] : false;
+      $visibility = isset($event['visibility']) ? $event['visibility'] : 'public';
+      
+      if ($type == 'normal') $filter = !$hangoutlink;
+      elseif ($type == 'hangout') $filter = $hangoutlink;
+      
+      if ( $author == 'self' ) $creator = isset( $event['creator']['self'] ) ? $event['creator']['self'] : 0;
+      
+      if ( $filter && $creator && ($visibility != 'private') ) { $i++;
+        $output .= '<div class="yghe-event">';
+        $output .= '<div class="yghe-creator"><a href="https://plus.google.com/' . $event['creator']['id'] . '" title="' . $event['creator']['displayName'] . '">' . $event['creator']['displayName'] . '</a> ' . googleplushangoutevent_ago($event['created'], $event['updated']) . '</div>';
+        $output .= '<div class="yghe-event-title"><a href="' . $event['htmlLink'] . '" title="' . $event['summary'] . '">' . $event['summary'] . '</a></div>';
+        $output .= '<div class="yghe-event-time">' . googleplushangoutevent_time($event['start']['dateTime'], $event['end']['dateTime'], 'shortcode') . '</div>';
+
+        if ($event['location']) {
+          $output .= '<div class="yghe-event-location"><a href="http://maps.google.com/?q=' . $event['location'] . '" title="' . $event['location'] . '">' . $event['location'] . '</a></div>';
+        } else {
+          $output .= '<div class="yghe-event-hangout"><a href="' . $event['hangoutLink'] . '" title="Google+ Hangout">Google+ Hangout</a></div>';
+        }
+
+        $output .= '<div class="yghe-event-description">'. $event['description'] . '</div>';
+        $output .= '</div>';
+
+        if ($limit == $i) break;
+      }
+    }
+  }
+  
+  if ($output == null) $output = 'No Event.';
+  
+  return $output;
+}
+add_shortcode( 'google+events', 'googleplushangoutevent_shortcode' );
+
+function googleplushangoutevent_time($startdate, $finishdate, $type) {
+  $diff = round(abs(strtotime($finishdate)-strtotime($startdate))/86400);
+  
+  $begindate = str_split($startdate, 19);
+  
+  $output = null;
+  
+  if ( $type == 'shortcode'  ) {
+    $timezone = googleplushangoutevent_timezone( $begindate[1] );
+    $output = date('D, F d, g:i A', strtotime($begindate[0])) . '&nbsp;' . $timezone;
+  } elseif ( $type == 'widget' ) {
+    $timezone = googleplushangoutevent_timezone( $begindate[1] );
+    $enddate = str_split($finishdate, 19);
+    $output = '<span>' . date('F jS Y', strtotime($begindate[0])) . '</span><br><span>' . date('g:i a', strtotime($begindate[0])) . '&nbsp;-&nbsp;' . date('g:i a', strtotime($enddate[0])) . '&nbsp;' . $timezone . '</span>';
+  }
+  
+  if ( $diff >= 1 ) {
+    $timezone = googleplushangoutevent_timezone( $begindate[1] );
+    $enddate = str_split($finishdate, 19);
+    if ( $type == 'shortcode' ) {
+      $output = date('D, F d, g:i A', strtotime($begindate[0])) . '&nbsp;' . $timezone . ' - ' . date('D, F d, g:i A', strtotime($enddate[0])) . '&nbsp;' . $timezone;
+    } elseif ( $type == 'widget' ) {
+      $output = '<span>' . date('F jS Y g:i a', strtotime($begindate[0])) . '&nbsp;to</span><br><span>' . date('F jS Y g:i a', strtotime($enddate[0])) . '&nbsp;' . $timezone . '</span>';
+    }
+  }
+  
+  return $output;
+}
+
+function googleplushangoutevent_ago($datetime1, $datetime2) {
+  $i = strtotime($datetime1);
+  $info = null;
+  
+  // difference in minutes
+  $diff = (strtotime($datetime2) - strtotime($datetime1)) / 60;
+  if ($diff > 1) {
+    $i = strtotime($datetime2);
+    $info = '&nbsp;(updated)';
+  }
+  
+  $m = time()-$i; $o='just now';
+  $t = array('year'=>31556926,'month'=>2629744,'week'=>604800, 'day'=>86400,'hour'=>3600,'minute'=>60,'second'=>1);
+  foreach($t as $u=>$s){
+    if ($s<=$m) {$v=floor($m/$s); $o="$v $u".($v==1?'':'s').' ago'; break;}
+  }
+  
+  $o = $o . $info;
+  
+  return $o;
+}
+
+function googleplushangoutevent_timezone( $time ) {
+  $output = null;
+  
+  if ( $time == '+01:00' ) $output = 'ECT'; // European Central Time
+  elseif ( $time == '+02:00' ) $output = 'EET'; // Eastern European Time
+  //elseif ( $time == '+02:00' ) $output = 'ART'; // (Arabic) Egypt Standard Time
+  elseif ( $time == '+03:00' ) $output = 'EAT'; // Eastern African Time
+  elseif ( $time == '+03:30' ) $output = 'MET'; // Middle East Time
+  elseif ( $time == '+04:00' ) $output = 'NET'; // Near East Time
+  elseif ( $time == '+05:00' ) $output = 'PLT'; // Pakistan Lahore Time
+  elseif ( $time == '+05:30' ) $output = 'IST'; // India Standard Time
+  elseif ( $time == '+06:00' ) $output = 'BST'; // Bangladesh Standard Time
+  elseif ( $time == '+07:00' ) $output = 'VST'; // Vietnam Standard Time
+  elseif ( $time == '+08:00' ) $output = 'CTT'; // China Taiwan Time
+  elseif ( $time == '+09:00' ) $output = 'JST'; // Japan Standard Time
+  elseif ( $time == '+09:30' ) $output = 'ACT'; // Australia Central Time
+  elseif ( $time == '+10:00' ) $output = 'AET'; // Australia Eastern Time
+  elseif ( $time == '+11:00' ) $output = 'SST'; // Solomon Standard Time
+  elseif ( $time == '+12:00' ) $output = 'NST'; // New Zealand Standard Time
+  elseif ( $time == '-11:00' ) $output = 'MIT'; // Midway Islands Time
+  elseif ( $time == '-10:00' ) $output = 'HST'; // Hawaii Standard Time
+  elseif ( $time == '-09:00' ) $output = 'AST'; // Alaska Standard Time
+  elseif ( $time == '-08:00' ) $output = 'PST'; // Pacific Standard Time
+  elseif ( $time == '-07:00' ) $output = 'PNT'; // Phoenix Standard Time
+  //elseif ( $time == '-07:00' ) $output = 'MST'; // Mountain Standard Time
+  elseif ( $time == '-06:00' ) $output = 'CST'; // Central Standard Time
+  elseif ( $time == '-05:00' ) $output = 'EST'; // Eastern Standard Time
+  //elseif ( $time == '-05:00' ) $output = 'IET'; // Indiana Eastern Standard Time
+  elseif ( $time == '-04:00' ) $output = 'PRT'; // Puerto Rico and US Virgin Islands Time
+  elseif ( $time == '-03:30' ) $output = 'CNT'; // Canada Newfoundland Time
+  elseif ( $time == '-03:00' ) $output = 'AGT'; // Argentina Standard Time
+  //elseif ( $time == '-03:00' ) $output = 'BET'; // Brazil Eastern Time
+  elseif ( $time == '-01:00' ) $output = 'CAT'; // Central African Time
+  
+  return $output;
+}
+
+/*add_action('init', 'xx');
+function xx() {
+  
+  $months = 1;
+
+  $timeMin = date('c'); // the date is today
+
+  $timeMin = date('c', strtotime("-".$months." month", strtotime($timeMin)));
+  
+  echo $timeMin;
+  die();
+}*/
